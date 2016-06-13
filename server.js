cd96083025184767b39910350100f0f0afe28885
Wasiq08 <s.wasiq.muhammad@gmail.com>
2016-06-13 23:27:21 +0500

    fixed port

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var config         = require('./config/config');
 //body parser to use for POST request
 server.use(bodyParser.json());
 
+
+var port = process.env.PORT || 8000;
+
 // parse server aplication/x-www-form-urlencoded
 server.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,7 +18,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json({ type: 'serverlication/vnd.api+json' }));
 
 // start server ===============================================
-server.listen(config.serverNetwork.pushServer_private.port);
-console.log(chalk.bgGreen('listening at: ',config.serverNetwork.pushServer_private.port));
+server.listen(port);
+console.log(chalk.bgGreen('listening at: ',port));
 
 require('./router')(server);
